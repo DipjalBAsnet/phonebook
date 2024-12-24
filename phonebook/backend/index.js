131e81f4d2746db1de2966658aa66059c254a0d8
unknown <dipjal5678@gmai.com>
2024-12-24 16:05:23 +0545

    added morgan middleware in application for logging

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
+app.use(express.json());
+
+app.use(morgan("tiny"));
 
 let persons = [
   {
@@ -24,7 +30,14 @@ let persons = [
   },
 ];
 
-app.use(express.json());
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+app.use(requestLogger);
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -90,6 +103,11 @@ app.post("/api/persons", (request, response) => {
 
   response.json(person);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
